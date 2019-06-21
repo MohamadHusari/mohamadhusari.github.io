8b2f0c714ca8390153eb57de0804d8c2a62b0709
Mohamad Husari <7osary@gmail.com>
2019-06-21 16:05:52 +0300

    Updates

---
+++ precache-manifest.4f01fffd80ed58b1eee699ad40fac06e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdca1721f42615f80e5d2e32fad4af1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010ddec7714381f94c91",
+    "url": "/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "e96d79107b21b8a15434",
+    "url": "/static/css/main.bbd5b2bd.chunk.css"
+  },
+  {
+    "revision": "010ddec7714381f94c91",
+    "url": "/static/js/2.07aac1f0.chunk.js"
+  },
+  {
+    "revision": "e96d79107b21b8a15434",
+    "url": "/static/js/main.c8f62536.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c3d61c03941b0bd8f13bb00f1dfe024",
+    "url": "/static/media/profile-pic1.0c3d61c0.png"
+  }
+]);
